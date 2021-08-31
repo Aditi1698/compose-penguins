1b7e5162125fb345678c8cb71611f44cc3a20793
Aditi1698 <87352653+Aditi1698@users.noreply.github.com>
2021-08-31 19:45:28 +0530

    Update utils.py

--- processr/utils.py
+++ processr/utils.py
@@ -3,18 +3,21 @@ import pickle
 from sklearn.naive_bayes import GaussianNB
 
 # define the class encodings and reverse encodings
-classes = {0: "Iris Setosa", 1: "Iris Versicolour", 2: "Iris Virginica"}
+classes = {0: "Adelie", 1: "Chinstrap", 2: "Gentoo"}
 r_classes = {y: x for x, y in classes.items()}
 
 # function to process data and return it in correct format
 def process_data(data):
     processed = [
         {
-            "sepal_length": d.sepal_length,
-            "sepal_width": d.sepal_length,
-            "petal_length": d.petal_length,
-            "petal_width": d.petal_width,
-            "flower_class": d.flower_class,
+            "species": d.species,
+            "island": d.island,
+            "culmen_length_mm": d.culmen_length_mm,
+            "culmen_depth_mm": d.culmen_depth_mm,
+            "flipper_length_mm": d.flipper_length_mm,
+            "body_mass_g": d.body_mass_g,
+            "sex": d.sex,
+
         }
         for d in data
     ]
